e398370ead3e2970ee151076105e5e93ad97b587
Han Nguyen <bibomx12er@gmail.com>
2022-04-25 03:30:43 +0700

    Update key stripe

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
     mongoURI: '',
     sendGridKey: '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',
-    stripeKey: ''
+    stripeKey: 'sk_test_51KsBxIAfr6M2lqwvSbyNetJpniIbzz9eh2e80jaRXbppjIKiGJRATi0yeMQ2hwNjBEzATd5LK9wuFF2uR8nRjK7a00Oxoa6Mwq'
 };
